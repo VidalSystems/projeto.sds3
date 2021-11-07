a7187d5faf261bc916e05ccabcdd221472da7d7d
VidalSystems <vidal_sistemas@outlook.com>
2021-11-07 15:58:54 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: String,
-    sum: number
+    sellerName: String;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
